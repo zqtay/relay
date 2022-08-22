753ded7df3cb47e8b9d7aaeb111bbb27a301c112
zqtay <zqtay96@gmail.com>
2022-08-22 23:47:23 +0800

    Add modal dialog

--- src/App.js
+++ src/App.js
@@ -4,48 +4,54 @@ import React, { useState, useRef } from 'react';
 
 import Game from "./game/game";
 import TitleBar from './components/UI/TitleBar';
+import Dialog from './components/UI/Dialog';
 
 const game = new Game();
 
 function App() {
-  const [currInputs, setCurrInputs] = useState([]);
+    const [currInputs, setCurrInputs] = useState([]);
   const [status, setStatus] = useState({res:null, data:null});
-  const inputRef = useRef(null);
+    const [showDialog, setShowDialog] = useState(false);
+    const inputRef = useRef(null);
 
-  function handleClick() {
-    setStatus(game.process(inputRef.current.value));
-    setCurrInputs(game.getCurrInputs().data);
-    inputRef.current.value = "";
-  }
-
-  function handleEnter(e) {
-    if(e.key === 'Enter') {
-      handleClick();   
+    function handleClick() {
+        setStatus(game.process(inputRef.current.value));
+        setCurrInputs(game.getCurrInputs().data);
+        inputRef.current.value = "";
     }
-  }
 
-  return (
-    <div className="App">
-      <header>
+    function handleEnter(e) {
+    if(e.key === 'Enter') {
+            handleClick();
+        }
+    }
+
+    return (
+        <div className="App">
+            <header>
         <TitleBar title="Relay" onClickHandlers={{info:null, newGame:null, settings:null}} />
-      </header>
-      <div>
-        <div>{game.getHints().data}</div>
-        <div>{`${currInputs}`}</div>
-        <div>{`${status.data}`}</div>
-        <div>
-          <input
-            ref={inputRef}
-            type="text"
-            id="message"
-            name="message"
-            onKeyDown={handleEnter}
-          />
+            </header>
+      <button type="button" className="btn btn-primary" onClick={() => setShowDialog(true)} >
+        Launch demo modal
+      </button>
+      <Dialog show={showDialog} title="info" content="content" btnConfirm={{name:"Confirm", onClick: () => setShowDialog(false)}} dismiss={() => setShowDialog(false)} />
+            <div>
+                <div>{game.getHints().data}</div>
+                <div>{`${currInputs}`}</div>
+                <div>{`${status.data}`}</div>
+                <div>
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        id="message"
+                        name="message"
+                        onKeyDown={handleEnter}
+                    />
+                </div>
+                <button onClick={handleClick}>Submit</button>
+            </div>
         </div>
-        <button onClick={handleClick}>Submit</button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
