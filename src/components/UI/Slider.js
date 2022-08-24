c9b68514ac145822eac8a928020665d5acf0dd5e
zqtay <zqtay96@gmail.com>
2022-08-25 01:13:18 +0800

    Add slider component

---
+++ src/components/UI/Slider.js
@@ -0,0 +1,21 @@
+import "./Slider.css";
+
+const Slider = ({ title, id = "", min, max, step = 1, defaultValue=min, _ref = null }) => {
+    const tickMarks = [];
+    let tick = min;
+    while (tick <= max) {
+        tickMarks.push(<span key={tick}>{tick}</span>);
+        tick += step;
+    }
+    return (
+        <>
+            <div className="slider-title text-start">{title}</div>
+            <input type="range" className="form-range slider" id={id} min={min} max={max} step={step} defaultValue={defaultValue} ref={_ref}></input>
+            <div className="slider-tickmarks">
+                {tickMarks}
+            </div>
+        </>
+    );
+}
+
+export default Slider;
