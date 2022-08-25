0d500ec49b1da850391d0a2018cdc623dae3ed12
zqtay <zqtay96@gmail.com>
2022-08-26 01:12:31 +0800

    Parse game settings from URL query
    Move to Board componenet

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import './App.css';
 
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 
-import Game from "./game/Game";
 import TitleBar from './components/UI/TitleBar';
 import NewPuzzleDialog from './components/Dialog/NewPuzzleDialog';
-
-const game = new Game();
+import Board from './components/Board/Board';
 
 function App() {
-    const [currInputs, setCurrInputs] = useState([]);
-    const [status, setStatus] = useState({ res: null, data: null });
     const [showDialog, setShowDialog] = useState(false);
-    const inputRef = useRef(null);
-
-    function handleClick() {
-        setStatus(game.process(inputRef.current.value));
-        setCurrInputs(game.getCurrInputs().data);
-        inputRef.current.value = "";
-    }
-
-    function handleEnter(e) {
-        if (e.key === 'Enter') {
-            handleClick();
-        }
-    }
 
     return (
         <div className="App">
             <header>
                 <TitleBar title="Relay" onClickHandlers={{ info: null, newGame: () => setShowDialog(true), settings: null }} />
             </header>
-            <NewPuzzleDialog show={showDialog} btnConfirm={{name:"OK", onClick: () => {inputRef.current.value = "g"; handleClick(); setShowDialog(false);}}} dismiss={() => setShowDialog(false)} />
-            <div>
-                <div>{game.getHints().data}</div>
-                <div>{`${currInputs}`}</div>
-                <div>{`${status.data}`}</div>
-                <div>
-                    <input
-                        ref={inputRef}
-                        type="text"
-                        id="message"
-                        name="message"
-                        onKeyDown={handleEnter}
-                    />
-                </div>
-                <button onClick={handleClick}>Submit</button>
-            </div>
+            <NewPuzzleDialog show={showDialog} dismiss={() => setShowDialog(false)} />
+            <Board />
         </div>
     );
 }
---
+++ src/components/Board/Board.js
@@ -0,0 +1,51 @@
+import { useEffect, useState, useRef, memo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import Game from "../../game/Game";
+const game = new Game();
+
+const Board = (props) => {
+    const [currInputs, setCurrInputs] = useState([]);
+    const [status, setStatus] = useState({ res: null, data: null });
+    const inputRef = useRef(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+    let puzzleSettings = null
+
+    useEffect(() => {
+        if ((puzzleSettings = searchParams.get("puzzle")) !== null) {
+            game.genPuzzleFromEncoded(puzzleSettings);
+            setCurrInputs(game.getCurrInputs().data);
+        }
+    }, []);
+
+    function handleClick() {
+        setStatus(game.process(inputRef.current.value));
+        setCurrInputs(game.getCurrInputs().data);
+        inputRef.current.value = "";
+    }
+
+    function handleEnter(e) {
+        if (e.key === 'Enter') {
+            handleClick();
+        }
+    }
+
+    return (
+        <div>
+            <div>{game.getHints().data}</div>
+            <div>{`${currInputs}`}</div>
+            <div>{`${status.data}`}</div>
+            <div>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    id="message"
+                    name="message"
+                    onKeyDown={handleEnter}
+                />
+            </div>
+            <button onClick={handleClick}>Submit</button>
+        </div>
+    );
+};
+
+export default memo(Board);
