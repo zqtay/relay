6b80dcf83bc5bfbf17ca578e293af0fd16d73f7b
zqtay <zqtay96@gmail.com>
2022-08-28 23:04:18 +0800

    Use core-js for polyfill

---
+++ src/polyfill.js
@@ -0,0 +1,4 @@
+// Manual edit babel-preset-react-app\create.js to use higher minor version of core-js
+// corejs: '3.25'; // Default is 3
+// https://github.com/facebook/create-react-app/issues/8875#issuecomment-617481499
+import 'core-js/features/array/at';
