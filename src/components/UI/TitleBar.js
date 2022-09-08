8f5632c009505bbabcc2fdfd489e48ece3c905a2
zqtay <zqtay96@gmail.com>
2022-09-09 00:44:35 +0800

    Add logo

--- src/components/UI/TitleBar.js
+++ src/components/UI/TitleBar.js
@@ -1,4 +1,5 @@
 import "./TitleBar.css"
+import logo from "../../logo.png";
 
 import { memo } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,9 +14,12 @@ const TitleBar = (props) => {
     return (
         <nav className="navbar navbar-expand bg-light">
             <div className="container">
-                <Link className="navbar-brand" to="/" onClick={reload}>{props.title}</Link>
+                <Link className="navbar-brand d-flex align-items-center" to="/" onClick={reload}>
+                    <img className="titlebar-logo" src={logo} />
+                    <span className="titlebar-brand-text">Relay</span>
+                </Link>
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ms-auto align-self-center">
+                    <ul className="navbar-nav ms-auto d-flex align-items-center">
                         <li className="nav-item ps-3">
                             <TitleBarButton icon="fa-circle-info" onClick={props.onClickHandlers.info} />
                         </li>
